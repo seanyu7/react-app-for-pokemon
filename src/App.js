404ae9b574993f4101a378ad6ff28aee93d33512
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-08-08 22:36:38 +0900

    🗃️ madea code to fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
 import "./App.css";
+import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
  const initialURL = "https://pokeapi.co/api/v2/pokemon/";
  /*このURLのことをエンドポイントと言う。またこのエンドポイントのURLを叩くことで、データを引っ張って来ることが出来る。*/ 
+
+useEffect(() => {
+  const fetchPokemonData = async () => {
+    //全てのポケモンのデータを取得する
+    let res = await getAllPokemon(initialURL);
+    console.log(res);
+}
+fetchPokemonData();
+},[]);
  
  return <div className="App"></div>
 }
