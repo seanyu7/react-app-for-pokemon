4ce9e2dd3e39a4e7ccd5f0d02d397b023fb92a4e
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-08-08 23:08:38 +0900

    ⚡️ made a loading function inside my app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState} from "react";
 import "./App.css";
 import { getAllPokemon } from "./utils/pokemon";
 
@@ -6,16 +6,22 @@ function App() {
  const initialURL = "https://pokeapi.co/api/v2/pokemon/";
  /*このURLのことをエンドポイントと言う。またこのエンドポイントのURLを叩くことで、データを引っ張って来ることが出来る。*/ 
 
+const [loading,setLoading] = useState(true);
+
 useEffect(() => {
   const fetchPokemonData = async () => {
     //全てのポケモンのデータを取得する
     let res = await getAllPokemon(initialURL);
     console.log(res);
+    setLoading(false);
 }
 fetchPokemonData();
 },[]);
  
- return <div className="App"></div>
+ return (<div className="App">
+  {loading ? (<h1>Loading...</h1> ): (<><h1>Pokemon data feched</h1></>)}
+ </div>
+ );
 }
 
 export default App;
