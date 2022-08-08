8571fb8e9d0b0e03d58e5ee5a799060b15ab35b9
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-08-08 23:58:59 +0900

    ⚡️ made getPokemon function to get each pokemons data

--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -6,3 +6,11 @@ export const getAllPokemon = (url) => {
   });
 };
 //promiseを使って、データを取得する場合はasyncとawaitを使う必要がある。
+
+export const getPokemon = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => resolve(data));
+  });
+};
