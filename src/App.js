7a441c86cc27071b10f1bd38b9ee4fb0e8ef6fcb
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-08-08 22:10:04 +0900

    🧐 adding end point URL wich give us data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+ const initialURL = "https://pokeapi.co/api/v2/pokemon/";
+ /*このURLのことをエンドポイントと言う。またこのエンドポイントのURLを叩くことで、データを引っ張って来ることが出来る。*/ 
+ 
+ return <div className="App"></div>
 }
 
 export default App;
