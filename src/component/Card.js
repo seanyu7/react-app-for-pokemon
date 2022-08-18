f80bac72e5485800c26b909f0e24baa8390ac2ac
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-08-19 00:56:09 +0900

    ⚡️ import more info about pokemon from AOI

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React from "react";
 
 /*make a card component to make it to make use of map function */
-const Card = ({pokemon}) => {
+const Card = ({ pokemon }) => {
   return (
-    <div className='card'>
-        <div className='card-image'>
-            <img src={pokemon.sprites.front_default} alt="the image of pokemon"/>
+    <div className="card">
+      <div className="card-image">
+        <img src={pokemon.sprites.front_default} alt="the image of pokemon" />
+      </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardTypes">
+        <div>タイプ</div>
+        {pokemon.types.map((type) => {
+          return (
+            <div>
+              <span className="typeName">{type.type.name}</span>
             </div>
+          );
+        })}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="title">Weight : {pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">Height : {pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">Ability : {pokemon.abilities[0].ability.name}</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
